1ddc6ac0e8bcf52d9b55e7d3c9bbe3c71e1fcc86
Gihun Ham <progh2@gmail.com>
2019-05-24 14:46:14 +0900

    #2 콤보박스 등 이벤트 처리 코드 구현

--- CB_Choice/Form1.Designer.cs
+++ CB_Choice/Form1.Designer.cs
@@ -42,6 +42,7 @@
             this.cbList.Name = "cbList";
             this.cbList.Size = new System.Drawing.Size(170, 20);
             this.cbList.TabIndex = 0;
+            this.cbList.SelectedIndexChanged += new System.EventHandler(this.CbList_SelectedIndexChanged);
             // 
             // txtList
             // 
@@ -49,6 +50,7 @@
             this.txtList.Name = "txtList";
             this.txtList.Size = new System.Drawing.Size(169, 21);
             this.txtList.TabIndex = 1;
+            this.txtList.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtList_KeyPress);
             // 
             // btnAdd
             // 
@@ -58,6 +60,7 @@
             this.btnAdd.TabIndex = 2;
             this.btnAdd.Text = "추가";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
             // 
             // lblResult
             // 
@@ -81,6 +84,7 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "메뉴 선택 추가";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- CB_Choice/Form1.cs
+++ CB_Choice/Form1.cs
@@ -11,9 +11,62 @@ namespace CB_Choice
 {
     public partial class Form1 : Form
     {
+        string[] SList = new string[]
+        {
+            "짜장면", "짬뽕", "라조기", "탕수육"
+        };
+        string OrgStr = "";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i = 0; i < this.SList.Length; i++)
+            {
+                this.cbList.Items.Add(this.SList[i]);
+            }
+            this.OrgStr = this.lblResult.Text;
+            if(this.cbList.Items.Count > 0)
+            {
+                this.cbList.SelectedIndex = 0;
+            }
+        }
+
+        private void CbList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.lblResult.Text = this.OrgStr + this.cbList.Text;
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            CheckInput();
+        }
+
+        private void TxtList_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+                CheckInput();
+                e.Handled = true;
+            }
+        }
+
+        private void CheckInput() {
+            if (this.txtList.Text == "")
+            {
+                MessageBox.Show("추가할 항목을 선택하세요", "알림",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                this.txtList.Focus();
+            }
+            else
+            {
+                this.cbList.Items.Add(this.txtList.Text);
+                this.txtList.Text = "";
+                this.txtList.Focus();
+            }
         }
     }
 }
